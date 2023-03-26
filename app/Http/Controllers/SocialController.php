33875121aaf1bbb00f735ec2df18b5618a9206e4
XirTech <john.darlucio022@gmail.com>
2023-03-26 23:16:01 +0800

    add env example

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -1,11 +1,11 @@
 <?php
 namespace App\Http\Controllers;
-use Illuminate\Http\Request;
-use App\Models\User;
-use Validator;
-use Socialite;
 use Exception;
-use Auth;
+use Validator;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 class SocialController extends Controller
 {
     public function facebookRedirect()
